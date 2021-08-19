2acab1c70ad0ea45869d7ea3f3418f2d868f424e
zhaoshuai <shuai.zhao@qlchat.com>
2021-08-19 08:32:19 +0800

    提交下代码

--- algorithm-thinking/src/main/java/com/zs/algorithm/common/MinArray.java
+++ algorithm-thinking/src/main/java/com/zs/algorithm/common/MinArray.java
@@ -22,9 +22,10 @@ public class MinArray {
         int right = numbers.length - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
+            //临界点在
             if (numbers[mid] < numbers[right]) {
                 right = mid;
-            } else if (numbers[mid] > numbers[left]) {
+            } else if (numbers[mid] > numbers[right]) {
                 left = mid + 1;
             } else {
                 right--;
