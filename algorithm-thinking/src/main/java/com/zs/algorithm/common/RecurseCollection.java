5901c7f57290d146135b571d1e747a4b379e7d49
zhaoshuai <shuai.zhao@qlchat.com>
2021-10-24 08:58:47 +0800

    链表+递归

--- algorithm-thinking/src/main/java/com/zs/algorithm/common/RecurseCollection.java
+++ algorithm-thinking/src/main/java/com/zs/algorithm/common/RecurseCollection.java
@@ -249,4 +249,49 @@ public class RecurseCollection {
         }
     }
 
+    /**
+     * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+     * <p>
+     * 有效括号组合需满足：左括号必须以正确的顺序闭合。
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：n = 3
+     * 输出：["((()))","(()())","(())()","()(())","()()()"]
+     * 示例 2：
+     * <p>
+     * 输入：n = 1
+     * 输出：["()"]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/generate-parentheses
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param n
+     * @return
+     */
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        generateParenthesis(result, n, 0, 0, new StringBuilder());
+        return result;
+    }
+
+    public void generateParenthesis(List<String> result, int n, int left, int right, StringBuilder sb) {
+        if (left == n && right == n) {
+            result.add(sb.toString());
+            return;
+        }
+        if (left < n) {
+            sb.append("(");
+            generateParenthesis(result, n, left + 1, right, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if (right < left) {
+            sb.append(")");
+            generateParenthesis(result, n, left, right + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
 }
