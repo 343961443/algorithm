e2da9e25ecb0134d41a1a82ab4a5894e55164b46
zhaoshuai <shuai.zhao@qlchat.com>
2022-03-03 20:39:50 +0800

    dp再来一遍

--- algorithm-thinking/src/main/java/com/zs/algorithm/common/RecurseCollection.java
+++ algorithm-thinking/src/main/java/com/zs/algorithm/common/RecurseCollection.java
@@ -294,4 +294,62 @@ public class RecurseCollection {
             sb.deleteCharAt(sb.length() - 1);
         }
     }
+
+    /**
+     * 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+     * <p>
+     * candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
+     * <p>
+     * 对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：candidates = [2,3,6,7], target = 7
+     * 输出：[[2,2,3],[7]]
+     * 解释：
+     * 2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+     * 7 也是一个候选， 7 = 7 。
+     * 仅有这两种组合。
+     * 示例 2：
+     * <p>
+     * 输入: candidates = [2,3,5], target = 8
+     * 输出: [[2,2,2,2],[2,3,3],[3,5]]
+     * 示例 3：
+     * <p>
+     * 输入: candidates = [2], target = 1
+     * 输出: []
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/combination-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        dsfCombinationSum(candidates, 0, candidates.length, target, list, res);
+        return res;
+
+    }
+
+    private void dsfCombinationSum(int[] candidates, int begin, int length, int target, LinkedList<Integer> list, List<List<Integer>> res) {
+        if (target < 0) {
+            return;
+        }
+
+        if (target == 0) {
+            res.add(new ArrayList(list));
+        }
+        for (int i = begin; i < length; i++) {
+            list.addLast(candidates[i]);
+            // 从i开始再来一遍
+            dsfCombinationSum(candidates, i, length, target - candidates[i], list, res);
+            list.removeLast();
+        }
+    }
 }
